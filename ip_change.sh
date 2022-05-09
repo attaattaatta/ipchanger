af4c8de2b7a78dbc7f1364c2653703297ee673a5
Pavel Andronov <atta.root@gmail.com>
2022-05-09 22:10:22 +0800

    Proceed without or wrong version ISP Panel
    Signed-off-by: Pavel Andronov <atta.root@gmail.com>

--- ip_change.sh
+++ ip_change.sh
@@ -16,12 +16,54 @@ SELF_NAME=$(basename "$0");
 if [[ -z "${1}" ]]; then printf "$R_C Old ip not set up$N_C\n  Usage $SELF_NAME old_ip new_ip\n"; exit 1; else printf "$R_C Old ip - $1$N_C\n"; fi;
 if [[ -z "${2}" ]]; then printf "$G_C New ip not set up$N_C\n  Usage $SELF_NAME old_ip new_ip\n"; exit 1; else printf "$G_C New ip - $2$N_C\n"; fi;
 
+
+proceed_without_isp() {
+	read -p "Continiue IP change without ISP panel DB modification [Y/n]" -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]
+		then
+			printf "\n$G_C  Backing up current network settings to /root/support/$TSTAMP$N_C\n";
+			\cp -Rf /etc/network* /root/support/$TSTAMP/;
+			\cp -Rf /etc/sysconfig/network* /root/support/$TSTAMP/;
+
+			printf "\n$G_C  Current network settings:$N_C\n\n";
+			echo "$(ip a)"; echo; echo "$(ip r)";
+
+			printf "\n$G_C  Starting ip change systemwide$N_C\n";
+			grep --devices=skip -RIil --exclude={*.run*,*random*} $1 /var/named* | xargs sed -i "s@$1@$2@gi";
+			grep --devices=skip -RIil --exclude={*.run*,*random*} $1 /var/lib/powerdns* | xargs sed -i "s@$1@$2@gi";
+			grep --devices=skip -RIil --exclude={*.run*,*random*} $1 /etc* | xargs sed -i "s@$1@$2@gi";
+
+			printf "\n$G_C  $1 -> $2 changed.$N_C\n";
+
+			if [ "$ISP5_RTG" = "1" ]; then 
+				printf "\n$R_C  Update ISP license, RUN manually: curl -X POST -F \"func=soft.edit\" -F \"elid=$ISP5_LITE_ELID\" -F \"out=text\" -F \"ip=$2\" -F \"sok=ok\" -ks \"https://my.ispsystem.com/billmgr\" -F \"authinfo=support@provider:password\" $N_C\n";
+				printf "\n$G_C  ISP Manager - https://ssh.hoztnode.net/?url=$2:1500/ispmgr $N_C\n";
+			fi;
+			
+			echo;
+			echo -n "  Default gateway need to be changed ? [y/n] ";
+			read answer;
+			if [ "$answer" != "${answer#[Yy]}" ] ;then
+				printf "\n$G_C  Ok. Change gateway manually and delete old ip $1 from Billing with reboot (do not check -billing only- checkbox). $N_C\n";
+				printf "\n$G_C  Good luck, bro ! $N_C";
+				exit 0;
+			fi;
+			printf "\n$R_C  Now delete old ip $1 from Billing with reboot (do not check -billing only- checkbox). $N_C";
+			printf "\n$G_C  And don't forget to check DNS afterwards. $N_C";
+			printf "\n$G_C  Good luck, bro ! $N_C";
+		else
+			printf "\n$G_C  Nothing is done. Come back, bro !$N_C\n";
+			exit 1
+	fi	
+}
+
 echo -n "  DO NOT delete $1 in Billing now ! Did you add $2 in the Billing ? [y/n] ";
 read answer;
 if [[ "$answer" != "${answer#[Yy]}" ]] ; then
 
         # check ISP4 panel
-        if [[ -f "/usr/local/ispmgr/bin/ispmgr" ]]; then printf "$R_C ISP 4 panel detected. Use your hands. Aborting.$N_C"; ISP5_RTG=0; sleep 2s; exit 1; fi;
+        if [[ -f "/usr/local/ispmgr/bin/ispmgr" ]]; then printf "$R_C ISP 4 panel detected.$N_C"; ISP5_RTG=0; sleep 2s; proceed_without_isp; fi;
 
         # vars
 	if [[ -f "/usr/local/mgr5/sbin/mgrctl" ]]
@@ -37,7 +79,7 @@ if [[ "$answer" != "${answer#[Yy]}" ]] ; then
         if [ -f "$ISP5_PANEL_FILE" ]; then shopt -s nocasematch;
                 case "$ISP5_LITE_LIC" in
                         *busines*)
-                                printf "\n$R_C Business panel license detected. Use your hands. Aborting.$N_C\n"; ISP5_RTG=0; sleep 2s; exit 1 ;;
+                                printf "\n$R_C Business panel license detected.$N_C\n"; ISP5_RTG=0; sleep 2s; proceed_without_isp;;
                         *lite*|*pro*|*host*)
                                 printf "\n$G_C Lite panel license detected.\n  Backing up db file$N_C\n"; ISP5_RTG=1; sleep 2s;
 
@@ -67,48 +109,21 @@ if [[ "$answer" != "${answer#[Yy]}" ]] ; then
                                 printf "\n$G_C  Update completed. Removing old ip - $1$N_C\n";
                                 $ISP5_PANEL_FILE -m ispmgr ipaddrlist.delete elid=$1 sok=ok;
                                 $ISP5_PANEL_FILE -m ispmgr exit
+				 sleep 5s
+				proceed_without_isp
                                 ;;	
                         *)
-                                printf "\n$R_C  Unknown panel license detected. Version: $ISP5_LITE_LIC. Aborting.$N_C\n"; ISP5_RTG=0; sleep 5s; exit 1 ;;
+                                printf "\n$R_C  Unknown panel license detected. Version: $ISP5_LITE_LIC.$N_C\n"; ISP5_RTG=0; sleep 5s; proceed_without_isp ;;
                 esac;
 
-        else printf "$R_C  No ISP5 panel detected.$N_C";
-
+        else 
+		printf "$R_C  No ISP5 panel detected.$N_C"
+		proceed_without_isp
         fi;
-
-        printf "\n$G_C  Backing up current network settings to /root/support/$TSTAMP$N_C\n";
-        \cp -Rf /etc/network* /root/support/$TSTAMP/;
-	\cp -Rf /etc/sysconfig/network* /root/support/$TSTAMP/;
-
-        printf "\n$G_C  Current network settings:$N_C\n\n";
-        echo "$(ip a)"; echo; echo "$(ip r)";
-
-        printf "\n$G_C  Starting ip change systemwide$N_C\n";
-	grep --devices=skip -RIil --exclude={*.run*,*random*} $1 /var/named* | xargs sed -i "s@$1@$2@gi";
-	grep --devices=skip -RIil --exclude={*.run*,*random*} $1 /var/lib/powerdns* | xargs sed -i "s@$1@$2@gi";
-	grep --devices=skip -RIil --exclude={*.run*,*random*} $1 /etc* | xargs sed -i "s@$1@$2@gi";
-
-	printf "\n$G_C  $1 -> $2 changed.$N_C\n";
-
-	if [ "$ISP5_RTG" = "1" ]; then 
-		printf "\n$R_C  Update ISP license, RUN manually: curl -X POST -F \"func=soft.edit\" -F \"elid=$ISP5_LITE_ELID\" -F \"out=text\" -F \"ip=$2\" -F \"sok=ok\" -ks \"https://my.ispsystem.com/billmgr\" -F \"authinfo=support@provider:password\" $N_C\n";
-		printf "\n$G_C  ISP Manager - https://ssh.hoztnode.net/?url=$2:1500/ispmgr $N_C\n";
-	fi;
 	
-	echo;
-	echo -n "  Default gateway need to be changed ? [y/n] ";
-	read answer;
-	if [ "$answer" != "${answer#[Yy]}" ] ;then
-		printf "\n$G_C  Ok. Change gateway manually and delete old ip $1 from Billing with reboot (do not check -billing only- checkbox). $N_C\n";
-		printf "\n$G_C  Good luck, bro ! $N_C";
+	else 
+		clear; 
+		printf "\n$G_C  Nothing is done. Come back, bro !$N_C\n";
 		exit 0;
-	fi;
-	printf "\n$R_C  Now delete old ip $1 from Billing with reboot (do not check -billing only- checkbox). $N_C";
-	printf "\n$G_C  And don't forget to check DNS afterwards. $N_C";
-	printf "\n$G_C  Good luck, bro ! $N_C";
-
-else 
-	clear; 
-	printf "\n$G_C  Nothing is done. Come back, bro !$N_C\n";
-	exit 0;
 fi;
+
