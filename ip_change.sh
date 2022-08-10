1e07a3bff362bbaa444c831cbe4e4b33bce4af6e
Pavel Andronov <atta.root@gmail.com>
2022-08-10 12:22:54 +0800

    no message
    Signed-off-by: Pavel Andronov <atta.root@gmail.com>

--- ip_change.sh
+++ ip_change.sh
@@ -31,10 +31,10 @@ proceed_without_isp() {
 			echo "$(ip a)"; echo; echo "$(ip r)";
 
 			printf "\n$G_C  Starting ip change systemwide$N_C\n";
-			grep --devices=skip -RIil --exclude={*.run*,*random*} ${args[0]} /var/named* | xargs sed -i "s@${args[0]}@${args[1]}@gi";
-			grep --devices=skip -RIil --exclude={*.run*,*random*} ${args[0]} /var/lib/powerdns* | xargs sed -i "s@${args[0]}@${args[1]}@gi";
-			grep --devices=skip -RIil --exclude={*.run*,*random*} ${args[0]} /etc* | xargs sed -i "s@${args[0]}@${args[1]}@gi";
-			grep --devices=skip -RIil --exclude={*.run*,*random*} ${args[0]} /home* | xargs sed -i "s@${args[0]}@${args[1]}@gi";
+			grep --no-messages --devices=skip -RIil --exclude={*.run*,*random*} ${args[0]} /var/named* | xargs sed -i "s@${args[0]}@${args[1]}@gi";
+			grep --no-messages --devices=skip -RIil --exclude={*.run*,*random*} ${args[0]} /var/lib/powerdns* | xargs sed -i "s@${args[0]}@${args[1]}@gi";
+			grep --no-messages --devices=skip -RIil --exclude={*.run*,*random*} ${args[0]} /etc* | xargs sed -i "s@${args[0]}@${args[1]}@gi";
+			grep --no-messages --devices=skip -RIil --exclude={*.run*,*random*} ${args[0]} /home* | xargs sed -i "s@${args[0]}@${args[1]}@gi";
 
 			printf "\n$G_C  ${args[0]} -> ${args[1]} changed.$N_C\n";
 
